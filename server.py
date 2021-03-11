b40e24185844e7cc37867bd26c2a30d08d4a7f43
test-t <fogidako@wmail2.com>
2021-03-11 15:48:55 +0300

    server.py changed 3

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# CHANGED v3
+
 import socket
 print('Server started!!!')
 sock = socket.socket()
@@ -15,8 +17,8 @@ while True:
     data = conn.recv(1024).decode()
     if not data:
         break
-    print('Server recieved the message from Client: ',data.upper(),'\n')
-    print('Server is sending the message to Client: ',data.upper())
+    print('Server recieved the message from Client: ',data,'\n')
     conn.send(data.upper().encode())
-
+	print('Server sended the message to Client: ',data.upper())
+	
 conn.close()
